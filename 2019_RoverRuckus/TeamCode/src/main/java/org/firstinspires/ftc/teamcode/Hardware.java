a1dea6870332cd674d427da52f3f402659acee85
Aidan <aidanf2020@gmail.com>
2018-09-16 21:34:17 -0400

    Added Basic Teleop FTC
    Added basic teleop for FTC Rover Ruckus game based off robot concept discussed at 9/15/2018 meeting.

--- 2019_RoverRuckus/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ 2019_RoverRuckus/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -7,8 +7,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Hardware {
 
     //-----------------------------------------Motors-----------------------------------------//
-    public DcMotor leftDrive    = null;
-    public DcMotor rightDrive   = null;
+    public DcMotor leftDrive1   = null;
+    public DcMotor leftDrive2   = null;
+    public DcMotor rightDrive1  = null;
+    public DcMotor rightDrive2  = null;
+    public DcMotor towerExtend1 = null;
+    public DcMotor towerExtend2 = null;
+    public DcMotor towerRotate  = null;
+    public DcMotor collectorIntake = null;
 
     //----------------------------------Local OpMode Members----------------------------------//
     HardwareMap hwMap = null;
@@ -20,23 +26,60 @@ public class Hardware {
         hwMap = ahwMap;
 
         //-------------------------------------Motors-------------------------------------//
-        leftDrive   = hwMap.get(DcMotor.class, "leftDrive");
-        rightDrive  = hwMap.get(DcMotor.class, "rightDrive");
+        leftDrive1      = hwMap.get(DcMotor.class, "leftDrive1");
+        leftDrive2      = hwMap.get(DcMotor.class, "leftDrive2");
+        rightDrive1     = hwMap.get(DcMotor.class, "rightDrive1");
+        rightDrive2     = hwMap.get(DcMotor.class, "rightDrive2");
+        towerExtend1    = hwMap.get(DcMotor.class, "towerExtend1");
+        towerExtend2    = hwMap.get(DcMotor.class, "towerExtend2");
+        towerRotate     = hwMap.get(DcMotor.class, "towerRotate");
+        collectorIntake = hwMap.get(DcMotor.class, "towerCollect");
 
         //-------------------------------Motor Configuration------------------------------//
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        towerExtend1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        towerExtend2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        towerRotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        collectorIntake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftDrive1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftDrive2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        towerExtend1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        towerExtend2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        towerRotate.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        collectorIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        // TODO: Confirm Motor Direction is Correct Before Running
+        leftDrive1.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive2.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive1.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive2.setDirection(DcMotor.Direction.REVERSE);
+        towerExtend1.setDirection(DcMotor.Direction.REVERSE);
+        towerExtend2.setDirection(DcMotor.Direction.REVERSE);
+        towerRotate.setDirection(DcMotor.Direction.REVERSE);
+        collectorIntake.setDirection(DcMotor.Direction.REVERSE);
 
-        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        towerExtend1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        towerExtend2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        towerRotate.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        collectorIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
+        leftDrive1.setPower(0);
+        leftDrive2.setPower(0);
+        rightDrive1.setPower(0);
+        rightDrive2.setPower(0);
+        towerExtend1.setPower(0);
+        towerExtend2.setPower(0);
+        towerRotate.setPower(0);
+        collectorIntake.setPower(0);
     }
 }
--- 2019_RoverRuckus/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ 2019_RoverRuckus/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -5,78 +5,96 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
 @TeleOp (name = "PINK Teleop")
-public class Teleop extends OpMode
-{
+public class Teleop extends OpMode {
 
-    /* Declare OpMode members. */
-    private Hardware robot = new Hardware(); // use the class created to define a Pushbot's hardware
-    private double leftJoystick, rightJoystick;
-    private double leftWheelsMotorCmd, rightWheelsMotorCmd;
+    // Declare OpMode Members
+    private Hardware robot = new Hardware();
+    private double  leftJoystickBase;
+    private double  rightJoystickBase;
+    private boolean rightBumperBase;
+    private boolean leftBumperBase;
+    private double  leftJoystickTower;
+    private double  rightJoystickTower;
+    private double  leftWheelsMotorCmd;
+    private double  rightWheelsMotorCmd;
+    private double  towerExtendCommand;
+    private double  towerRotateCommand;
+    private double  towerCollectCmd;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+    // Code to run ONCE when the driver hits INIT
     @Override
-    public void init ()
-    {
-        /* Initialize the hardware variables.
-         * The init() method of the hardware class does all the work here
-         */
+    public void init () {
+        // Initialize the hardware variables.
         robot.init(hardwareMap);
 
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Driver");    //
+        // Send telemetry message to signify robot waiting
+        telemetry.addData("Say", "Hello Driver");
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
+    // Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
     @Override
-    public void init_loop ()
-    {
+    public void init_loop () {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+    // Code to run ONCE when the driver hits PLAY
     @Override
-    public void start ()
-    {
+    public void start () {
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
-    public void loop ()
-    {
+    public void loop () {
+
         // BASE CONTROL /////////////////////////////////////////////////////
-        // Run wheels in tank mode (The joystick is negative when pushed forward, so negate it)
-        leftJoystick = -gamepad1.left_stick_y;
-        rightJoystick = -gamepad1.right_stick_y;
+        leftJoystickBase = -gamepad1.left_stick_y;
+        rightJoystickBase = -gamepad1.right_stick_y;
 
-        if (gamepad1.left_trigger > 0.2)
-        {
-            leftWheelsMotorCmd = leftJoystick * 1.0;
-            rightWheelsMotorCmd = rightJoystick * 1.0;
+        if (gamepad1.left_trigger > 0.2) {
+            leftWheelsMotorCmd = leftJoystickBase * 1.0;
+            rightWheelsMotorCmd = rightJoystickBase * 1.0;
         }
-        else
-        {
-            leftWheelsMotorCmd = leftJoystick * 0.7;
-            rightWheelsMotorCmd = rightJoystick * 0.7;
+        else {
+            leftWheelsMotorCmd = leftJoystickBase * 0.7;
+            rightWheelsMotorCmd = rightJoystickBase * 0.7;
         }
 
-        // Set powers and positions
-        robot.leftDrive.setPower(leftWheelsMotorCmd);
-        robot.rightDrive.setPower(rightWheelsMotorCmd);
+        robot.leftDrive1.setPower(leftWheelsMotorCmd);
+        robot.leftDrive2.setPower(leftWheelsMotorCmd);
+        robot.rightDrive1.setPower(rightWheelsMotorCmd);
+        robot.rightDrive2.setPower(rightWheelsMotorCmd);
+
+
+        // TOWER CONTROL /////////////////////////////////////////////////////
+        leftJoystickTower = -gamepad2.left_stick_y;
+        rightJoystickTower = -gamepad2.right_stick_y;
+
+        towerExtendCommand = leftJoystickTower;
+        towerRotateCommand = rightJoystickTower;
+
+        robot.towerExtend1.setPower(towerExtendCommand);
+        robot.towerExtend2.setPower(towerExtendCommand);
+        robot.towerRotate.setPower(towerRotateCommand);
+
+
+        // COLLECTOR CONTROL /////////////////////////////////////////////////////
+        rightBumperBase = gamepad1.right_bumper;
+        leftBumperBase = gamepad1.left_bumper;
+
+        if (rightBumperBase) {
+            towerCollectCmd = 1;
+        }
+        else if (leftBumperBase) {
+            towerCollectCmd = -1;
+        }
+        else {
+            towerCollectCmd = 0;
+        }
+
+        robot.collectorIntake.setPower(towerCollectCmd);
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
+    // Code to run ONCE after the driver hits STOP
     @Override
-    public void stop ()
-    {
+    public void stop () {
     }
-
 }
